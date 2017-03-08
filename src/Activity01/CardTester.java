1c597132dbec172f835414e74b6c3acdef498f34
2006627 <2006627@AL-105-08.student.acalanes.k12.ca.us>
2017-03-08 13:21:25 -0800

    Activity 1 - Complete

--- src/Activity01/CardTester.java
+++ src/Activity01/CardTester.java
@@ -1,3 +1,5 @@
+package Activity01;
+
 /**
  * This is a class that tests the Card class.
  */
@@ -9,5 +11,15 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+                Card one = new Card("Ace", "Clubs", 5);
+                Card two = new Card("Queen", "Spades", 7);
+                Card three = new Card("Ace", "Clubs", 5);
+                
+                System.out.println(one);
+                System.out.println(two);
+                System.out.println(three);
+                
+                System.out.println(one.matches(two));
+                System.out.println(one.matches(three));
 	}
 }
